cbc376ab9c43f7aa9b7ba4f1e137ef2f6cc123e6
Balou9 <emmanuel.owosekun@gmail.com>
2018-09-24 14:55:10 +0200

    seminewline failing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Transform } = require('stream')
 const util = require('util')
 
+const isSemiNewLine = require('./lib/is.js')
+
 function Nosemi(options) {
   if (!(this instanceof Nosemi)) {
     return new Nosemi(options)
@@ -17,8 +19,15 @@ Nosemi.prototype._semiout = function semiout (n, chunk) {
   return buf
 }
 
+Nosemi.prototype._semiout2 = function _semiout2 (n, chunk) {
+  var buf = Buffer.alloc(n)
+  for (var i = 0; i < n; i++)
+    buf[i] = (isSemiNewLine(chunk[i]) === true) ? null : chunk[i]
+  return buf
+}
+
 Nosemi.prototype._transform = function (chunk, _, next) {
-  this.push(this._semiout(chunk.length, chunk))
+  this.push(this._semiout2(chunk.length, chunk))
   next()
 }
 
--- test.js
+++ test.js
@@ -1,10 +1,17 @@
 const tape = require('tape')
 const Nosemi = require('./index.js')
+const isSemiNewLine = require('./lib/is.js')
 const fs = require('fs')
 const rs = fs.createReadStream('./semi.js')
 
-tape('nosemi - true', function (t) {
+tape('nosemi - pass', function (t) {
   const nosemi = new Nosemi()
   t.true(rs.pipe(nosemi).pipe(process.stdout))
+  t.end()
+})
+
+tape('isSemiNewLine - pass', function (t) {
+  const str = ';;;;wyha419hy;'
+  t.true(isSemiNewLine.bind(null, str))
   t.end()
 })
