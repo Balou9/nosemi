d216a106edeeef9baaee79c605bb17bf3b50c562
Balou9 <emmanuel.owosekun@gmail.com>
2018-11-30 09:21:31 +0100

    replace with empty buffer

--- semi.js
+++ semi.js
@@ -1,20 +1,7 @@
-var fs = require('fs');
-var zlib = require('zlib');
-
-var gzip = zlib.createGzip();
-var rstream = fs.createReadStream('myfile.txt');
-var wstream = fs.createWriteStream('myfile.txt.gz');
-
-rstream
-  .pipe(gzip)
-  .pipe(wstream)
-  .on('finish', function () {
-    console.log('done compressing');
-  });
-
-
 var result = [];
 
 for (var i = 0; i < 10; i++) {
   result.push(i)
 };
+
+console.log(result)
